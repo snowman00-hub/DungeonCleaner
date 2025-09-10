f2b03e1e974bdb7a78d0f1ce95f0c25c33bd40c1
윤준모 <ojm01088021296@gmail.com>
2025-09-10 17:51:53 +0900

    csv 테스트

---
+++ DungeonCleaner/Assets/Scripts/Defines.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using static Unity.VisualScripting.Icons;
+public enum Languages
+{
+    Korean,
+}
+
+public class StringIds
+{
+    public static readonly string Test = "Test";
+}
+
+public static class DataTableIds
+{
+    public static readonly string[] StringTableIds =
+    {
+        "StringTableKr",
+    };
+    public static string String => StringTableIds[(int)Variables.Language];
+
+    public static readonly string Item = "ItemTable";
+}
+public static class Variables
+{
+    public static Languages Language = Languages.Korean;
+}
