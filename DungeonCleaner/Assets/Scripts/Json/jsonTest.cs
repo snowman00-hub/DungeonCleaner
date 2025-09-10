528bf02fdba7b9df256a20efc845668e9696001b
윤준모 <ojm01088021296@gmail.com>
2025-09-10 22:50:22 +0900

    테스트

---
+++ DungeonCleaner/Assets/Scripts/Json/jsonTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class jsonTest : MonoBehaviour
+{
+    private void Update()
+    {
+        // unity remote할땐 키입력 안받음
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("asdf");
+            SaveLoadManager.Data.PlayerName = "TEST";
+            SaveLoadManager.Save();            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SaveLoadManager.Load();
+
+            Debug.Log(SaveLoadManager.Data.PlayerName);
+        }
+    }
+}
