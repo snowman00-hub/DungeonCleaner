71a496b7fb9c1abbd7cbb331ea586156b59cb604
윤준모 <ojm01088021296@gmail.com>
2025-09-12 23:30:37 +0900

    적 생성 최적화 및 에셋 적용

--- DungeonCleaner/Assets/Enemys/Scripts/EnemyHealth.cs
+++ DungeonCleaner/Assets/Enemys/Scripts/EnemyHealth.cs
@@ -2,19 +2,35 @@
 using UnityEngine;
 using UnityEngine.Audio;
 
+public enum EnemyName
+{
+    Mushroom,
+}
+
 public class EnemyHealth : LivingEntity
 {
+    public static readonly int hashHurt = Animator.StringToHash("Hurt");
+
+    public EnemyName enemyName;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
     public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
     {
         if (IsDead)
             return;
 
         base.OnDamage(damage, hitPoint, hitNormal);
+        animator.SetTrigger(hashHurt);
     }
 
     protected override void Die()
     {
         base.Die();
-        Destroy(gameObject);
     }
 }
--- DungeonCleaner/Assets/Enemys/Scripts/EnemySpawner.cs
+++ DungeonCleaner/Assets/Enemys/Scripts/EnemySpawner.cs
@@ -1,35 +1,51 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     public float minRadius = 5f;
     public float maxRadius = 7f;
     public int spawnCount = 10;
-    public GameObject enemyPrefab;
+    public List<GameObject> normalMonsters;
+    public List<GameObject> miniBossMonsters;
+    public GameObject bossMonster;
 
     private Transform player;
+    private Dictionary<EnemyName, Queue<GameObject>> monsterPools = new Dictionary<EnemyName, Queue<GameObject>>();
+    private int normalMonsterCount = 0;
 
     private void Start()
     {
         player = GameObject.FindWithTag(Tag.Player).transform;
+        Init();
     }
 
     private void Update()
     {
 #if UNITY_EDITOR
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            SpawnEnemy(minRadius, maxRadius, spawnCount, enemyPrefab);
+            SpawnEnemy(EnemyName.Mushroom, spawnCount, minRadius, maxRadius);
         }
 #endif
     }
 
-    public void SpawnEnemy(float minRadius, float maxRadius, int count, GameObject prefab)
+    public void SpawnEnemy(EnemyName name, int count, float minRadius, float maxRadius)
     {
-        for(int i=0;i<count;i++)
+        for (int i = 0; i < count; i++)
         {
             var spawnPos = GetRandomPositionInRing3D(player.position, minRadius, maxRadius);
-            Instantiate(prefab, spawnPos, Quaternion.identity, transform);
+            var monster = monsterPools[name].Dequeue();
+            monster.transform.position = spawnPos;
+            monster.SetActive(true);
+            normalMonsterCount++;
+        }
+
+        if(monsterPools[name].Count < 100)
+        {
+            var go = monsterPools[name].Peek();
+            ExpandMonsterPool(name, go);
         }
     }
 
@@ -42,5 +58,34 @@ public class EnemySpawner : MonoBehaviour
         float z = center.z + radius * Mathf.Sin(angle);
 
         return new Vector3(x, center.y, z);
+    }
+
+    private void Init()
+    {
+        foreach (var monster in normalMonsters)
+        {
+            var name = monster.GetComponent<EnemyHealth>().enemyName;
+            monsterPools[name] = new Queue<GameObject>();
+            ExpandMonsterPool(name, monster);
+        }
+    }
+
+    private void ExpandMonsterPool(EnemyName name, GameObject monster)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            var go = Instantiate(monster, transform);
+            go.SetActive(false);
+
+            var enemyHealth = go.GetComponent<EnemyHealth>();
+            enemyHealth.OnDeath += () =>
+            {
+                monsterPools[name].Enqueue(go);
+                go.SetActive(false);
+                normalMonsterCount--;
+            };
+
+            monsterPools[name].Enqueue(go);
+        }
     }
 }
