54a22b95b729f09621e7d13149eb50881e0545ac
윤준모 <ojm01088021296@gmail.com>
2025-09-14 15:43:48 +0900

    스킬 테스트 적용

--- DungeonCleaner/Assets/Skills/Scripts/SkillManager.cs
+++ DungeonCleaner/Assets/Skills/Scripts/SkillManager.cs
@@ -1,16 +1,77 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class SkillManager : MonoBehaviour
 {
-    public GameObject skillOne;
+    public List<Skill> skills;
+
+    private Dictionary<string, Queue<GameObject>> skillPools = new Dictionary<string, Queue<GameObject>>();
+    private int poolSize = 10;
+
+    private void Awake()
+    {
+        foreach (var skill in skills)
+        {
+            var queue = new Queue<GameObject>();
+            for (int i = 0; i < poolSize; i++)
+            {
+                GameObject obj = Instantiate(skill.gameObject);
+                obj.SetActive(false);
+                queue.Enqueue(obj);
+
+                var sk = obj.GetComponent<Skill>();
+                sk.OnUsed += () =>
+                {
+                    queue.Enqueue(obj);
+                    obj.SetActive(false);
+                };
+            }
+
+            skillPools[skill.skillName] = queue;
+        }
+    }
 
     private void Update()
     {
-#if UNITY_EDITOR
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        foreach (var skill in skills)
         {
-            Instantiate(skillOne, transform.position, Quaternion.identity);
+            if (skill.currentCoolDown > 0)
+            {
+                skill.currentCoolDown -= Time.deltaTime;
+            }
+            else
+            {
+                // 쿨타임이 다 됐으면 스킬 발동
+                UseSkill(skill);
+                skill.currentCoolDown = skill.coolDown;
+            }
         }
-#endif
+    }
+
+    private void UseSkill(Skill skill)
+    {
+        if (skillPools.TryGetValue(skill.skillName, out var queue))
+        {
+            if (queue.Count == 0)
+            {
+                for (int i = 0; i < poolSize; i++)
+                {
+                    GameObject go = Instantiate(skill.gameObject);
+                    go.SetActive(false);
+                    queue.Enqueue(go);
+
+                    var sk = go.GetComponent<Skill>();
+                    sk.OnUsed += () =>
+                    {
+                        queue.Enqueue(go);
+                        go.SetActive(false);
+                    };
+                }
+            }
+
+            var temp = queue.Dequeue().gameObject;
+            temp.transform.position = transform.position;
+            temp.SetActive(true);
+        }
     }
 }
